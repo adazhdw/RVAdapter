b970742f49c43abcf97273415063b9136d83848a
adazhdw <dgz102486@163.com>
2020-12-18 18:01:48 +0800

    add databinding module

---
+++ adapter-binding/src/main/java/com/adazhdw/adapter/binding/DataBindingViewHolder.kt
@@ -0,0 +1,11 @@
+package com.adazhdw.adapter.binding
+
+import androidx.databinding.ViewDataBinding
+import com.adazhdw.adapter.core.DefaultViewHolder
+
+/**
+ * author：adazhdw
+ * date-time：2020/12/18 17:46
+ * description：
+ **/
+class DataBindingViewHolder(binding: ViewDataBinding) : DefaultViewHolder(binding.root)
